79c3da3aa64a470ddf3d7fb2cef17e5b7a4fc631
Viktor Kertesz <vkertesz2@gmail.com>
2018-03-12 14:24:11 +0100

    cleanup code

--- clicol/cm_common.py
+++ clicol/cm_common.py
@@ -26,39 +26,39 @@ def init(ct):
         # Privileged prompt
         # \1: actual prompt
         # \2: anything after prompt (usually when hitting ?
-        [5,"prompt","",re.compile(r"^((?:[\b]+\ *[\b]*)?[a-zA-Z0-9\/_@\-\(\)]+#)(.*)$",flags=re.M), ct['privprompt']+r"\1"+ct['default']+r"\2",BREAK],
+        [5,"prompt","",re.compile(r"^((?:[\b]+\ *[\b]*)?[a-zA-Z0-9\/_@\-\(\)]+#)(.*)$"), "".join((ct['privprompt'],r"\1",ct['default'],r"\2")),BREAK],
         # Non-privileged prompt
-        [5,"prompt","",re.compile(r"^((?:[\b]+\ *[\b]*)?[a-zA-Z0-9\/_@\-\(\)]+(?:>|\$))(.*)$",flags=re.M), ct['nonprivprompt']+r"\1"+ct['default']+r"\2",BREAK],
+        [5,"prompt","",re.compile(r"^((?:[\b]+\ *[\b]*)?[a-zA-Z0-9\/_@\-\(\)]+(?:>|\$))(.*)$"), "".join((ct['nonprivprompt'],r"\1",ct['default'],r"\2")),BREAK],
 
         # Pager (more)
         # CONT because sometimes there is output after 'more'
-        [5,"more","",re.compile(r"^([\b ]*[<-]+\ *\(?[mM][oO][rR][eE](?:\ [0-9]+\%)?\)?\ *[->]+\ ?)(.*)$",flags=re.M), ct['pager']+r"\1"+ct['default']+r"\2",CONT],
+        [5,"more","",re.compile(r"^([\b ]*[<-]+\ *\(?[mM][oO][rR][eE](?:\ [0-9]+\%)?\)?\ *[->]+\ ?)(.*)$"), "".join((ct['pager'],r"\1",ct['default'],r"\2")),CONT],
 
         #Traffic
-        [10,"","",re.compile(BOS+r"(drop.*)(\b[1-9][0-9]*\b )((?:packets|pkts)\/sec)\b",flags=re.M),r"\1"+ct['lowalert']+r"\2"+ct['default']+r"\3",CONT],
-        [10,"","",re.compile(BOS+r"([1-9][0-9]* )((?:bits|bytes|pkts|packets)\/sec)\b",flags=re.M),ct['trafficrate']+r"\1"+ct['default']+r"\2",CONT],
+        [10,"","",re.compile(BOS+r"(drop.*)(\b[1-9][0-9]*\b )((?:packets|pkts)\/sec)\b"), "".join((r"\1",ct['lowalert'],r"\2",ct['default'],r"\3")),CONT],
+        [10,"","",re.compile(BOS+r"([1-9][0-9]* )((?:bits|bytes|pkts|packets)\/sec)\b"), "".join((ct['trafficrate'],r"\1",ct['default'],r"\2")),CONT],
 
         #MAC address
-        [10,"","",re.compile(BOS+r"(?<![:-])\b((?:[a-fA-F0-9]{2}[:-]){5}[a-fA-F0-9]{2})\b(?![:-])",flags=re.M), ct['address']+r"\1"+ct['default'],CONT],
-        [10,"","",re.compile(BOS+r"(?<!\.)\b((?:[a-fA-F0-9]{4}\.){2}[a-fA-F0-9]{4})\b(?!\.)",flags=re.M), ct['address']+r"\1"+ct['default'],CONT],
+        [10,"","",re.compile(BOS+r"(?<![:-])\b((?:[a-fA-F0-9]{2}[:-]){5}[a-fA-F0-9]{2})\b(?![:-])"), "".join((ct['address'],r"\1",ct['default'])),CONT],
+        [10,"","",re.compile(BOS+r"(?<!\.)\b((?:[a-fA-F0-9]{4}\.){2}[a-fA-F0-9]{4})\b(?!\.)"), "".join((ct['address'],r"\1",ct['default'])),CONT],
         #IPv4 address
-        [10,"","",re.compile(BOS+r"(?<![0-9]\.)\b((?:[0-9]{1,3}\.){3}[0-9]{1,3}(?:\/[0-9]{1,2})?)\b(?!\.[0-9])",flags=re.M), ct['address']+r"\1"+ct['default'],CONT],
+        [10,"","",re.compile(BOS+r"(?<![0-9]\.)\b((?:[0-9]{1,3}\.){3}[0-9]{1,3}(?:\/[0-9]{1,2})?)\b(?!\.[0-9])"), "".join((ct['address'],r"\1",ct['default'])),CONT],
         #IPv6 address
-        [10,"","",re.compile(BOS+r"((?:(?:(?:[0-9A-Fa-f]{1,4}:){7}(?:[0-9A-Fa-f]{1,4}|:))|(?:(?:[0-9A-Fa-f]{1,4}:){6}(?::[0-9A-Fa-f]{1,4}|(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3})|:))|(?:(?:[0-9A-Fa-f]{1,4}:){5}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,2})|:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3})|:))|(?:(?:[0-9A-Fa-f]{1,4}:){4}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,3})|(?:(?::[0-9A-Fa-f]{1,4})?:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(?:(?:[0-9A-Fa-f]{1,4}:){3}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,4})|(?:(?::[0-9A-Fa-f]{1,4}){0,2}:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(?:(?:[0-9A-Fa-f]{1,4}:){2}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,5})|(?:(?::[0-9A-Fa-f]{1,4}){0,3}:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(?:(?:[0-9A-Fa-f]{1,4}:){1}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,6})|(?:(?::[0-9A-Fa-f]{1,4}){0,4}:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(?::(?:(?:(?::[0-9A-Fa-f]{1,4}){1,7})|(?:(?::[0-9A-Fa-f]{1,4}){0,5}:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:)))(?:\/[0-9]{1,3})?)\b",flags=re.M), ct['address']+r"\1"+ct['default'],CONT],
+        [10,"","",re.compile(BOS+r"((?:(?:(?:[0-9A-Fa-f]{1,4}:){7}(?:[0-9A-Fa-f]{1,4}|:))|(?:(?:[0-9A-Fa-f]{1,4}:){6}(?::[0-9A-Fa-f]{1,4}|(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3})|:))|(?:(?:[0-9A-Fa-f]{1,4}:){5}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,2})|:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3})|:))|(?:(?:[0-9A-Fa-f]{1,4}:){4}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,3})|(?:(?::[0-9A-Fa-f]{1,4})?:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(?:(?:[0-9A-Fa-f]{1,4}:){3}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,4})|(?:(?::[0-9A-Fa-f]{1,4}){0,2}:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(?:(?:[0-9A-Fa-f]{1,4}:){2}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,5})|(?:(?::[0-9A-Fa-f]{1,4}){0,3}:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(?:(?:[0-9A-Fa-f]{1,4}:){1}(?:(?:(?::[0-9A-Fa-f]{1,4}){1,6})|(?:(?::[0-9A-Fa-f]{1,4}){0,4}:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:))|(?::(?:(?:(?::[0-9A-Fa-f]{1,4}){1,7})|(?:(?::[0-9A-Fa-f]{1,4}){0,5}:(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}))|:)))(?:\/[0-9]{1,3})?)\b"), "".join((ct['address'],r"\1",ct['default'])),CONT],
 
         #Interface description
-        [10,"","",re.compile(BOL+r"(\ *[dD]escription:?\ .+)$",flags=re.M),r"\1"+ct['description']+r"\2"+ct['default'],BREAK],
+        [10,"","",re.compile(BOL+r"(\ *[dD]escription:?\ .+)$"), "".join((r"\1",ct['description'],r"\2",ct['default'])),BREAK],
         #no shutdown
-        [10,"","",re.compile(r"\b(no shutdown)\b",flags=re.M),ct['good']+r"\1"+ct['default'],CONT],
-        [10,"","",re.compile(r"\b(?<!no )(shutdown)\b",flags=re.M),ct['alert']+r"\1"+ct['default'],CONT],
+        [10,"","",re.compile(r"\b(no shutdown)\b"), "".join((ct['good'],r"\1",ct['default'])),CONT],
+        [10,"","",re.compile(r"\b(?<!no )(shutdown)\b"), "".join((ct['alert'],r"\1",ct['default'])),CONT],
 
         # admin down
-        [10,"","",re.compile(BOS+r"(admin(?:istratively)? down)",flags=re.M),ct['alert']+r"\1"+ct['default'],CONT],
-        [10,"","",re.compile(BOS+r"(disabled|\b[dD]own\b)",flags=re.M),ct['alert']+r"\1"+ct['default'],CONT],
-        [10,"","",re.compile(BOS+r"(enabled|\b[uU]p\b)",flags=re.M),ct['good']+r"\1"+ct['default'],CONT],
+        [10,"","",re.compile(BOS+r"(admin(?:istratively)? down)"), "".join((ct['alert'],r"\1",ct['default'])),CONT],
+        [10,"","",re.compile(BOS+r"(disabled|\b[dD]own\b)"), "".join((ct['alert'],r"\1",ct['default'])),CONT],
+        [10,"","",re.compile(BOS+r"(enabled|\b[uU]p\b)"), "".join((ct['good'],r"\1",ct['default'])),CONT],
 
 
         #banner
-        [10,"","",re.compile(BOL+r"( *\*[\*\ \t]+.*\*\ *[\r\n]?)$",flags=re.M),r"\1"+ct['comment']+r"\2"+ct['default'],BREAK],
+        [10,"","",re.compile(BOL+r"( *\*[\*\ \t]+.*\*\ *[\r\n]?)$"), "".join((r"\1",ct['comment'],r"\2",ct['default'])),BREAK],
     ]
     return cmap
--- clicol/cm_juniper.py
+++ clicol/cm_juniper.py
@@ -22,22 +22,22 @@ def init(ct):
         #priority, effect, dependency, regex, replacement, break, [debug]
 
         #Juniper prompt user@hostname>
-        [4,"prompt","",re.compile(r"(?i)^((?:[\b]+\ *[\b]*)?[a-z0-9\/_\-]+@[a-z0-9\/_\-]*(?:>|\$) )(.*)$",flags=re.M), ct['privprompt']+r"\1"+ct['default']+r"\2",BREAK],
+        [4,"prompt","",re.compile(r"(?i)^((?:[\b]+\ *[\b]*)?[a-z0-9\/_\-]+@[a-z0-9\/_\-]*(?:>|\$) )(.*)$"), "".join((ct['privprompt'],r"\1",ct['default'],r"\2")),BREAK],
 
         # interface names
-        [15,"","",re.compile(BOS+r"\b(?<![\(\)\[\]\\\/.-:])((?:(?:(?:[gx]e|gr|ip|[lmuv]t|p[de]|pf[eh]|lc|lsq|sp)-|ae|em|fxp|lo|lis|me|pip|pp)[0-9]{1,4}|(?:reth|irb|cbp|lsi|mtun|pim[de]|tap|dsc|demux)[0-9]?)(?:[\/.][0-9]{1,5})*)\b(?! -)",flags=re.M),ct['interface']+r"\1"+ct['default'],CONT],
+        [15,"","",re.compile(BOS+r"\b(?<![\(\)\[\]\\\/.-:])((?:(?:(?:[gx]e|gr|ip|[lmuv]t|p[de]|pf[eh]|lc|lsq|sp)-|ae|em|fxp|lo|lis|me|pip|pp)[0-9]{1,4}|(?:reth|irb|cbp|lsi|mtun|pim[de]|tap|dsc|demux)[0-9]?)(?:[\/.][0-9]{1,5})*)\b(?! -)"), "".join((ct['interface'],r"\1",ct['default'])),CONT],
 
         #BGP
         # Juniper show bgp sum
         #Groups: 8 Peers: 17 Down peers: 0
-        [20,"","",re.compile(BOL+r"(Groups: [0-9]+ Peers: [0-9]+ Down peers: )(0)",flags=re.M),r"\1\2"+ct['good']+r"\3"+ct['default'],CLEAR],
-        [20,"","",re.compile(BOL+r"(Groups: [0-9]+ Peers: [0-9]+ Down peers: )([0-9]+)",flags=re.M),r"\1\2"+ct['alert']+r"\3"+ct['default'],CLEAR],
+        [20,"","",re.compile(BOL+r"(Groups: [0-9]+ Peers: [0-9]+ Down peers: )(0)"), "".join((r"\1\2",ct['good'],r"\3",ct['default'])),CLEAR],
+        [20,"","",re.compile(BOL+r"(Groups: [0-9]+ Peers: [0-9]+ Down peers: )([0-9]+)"), "".join((r"\1\2",ct['alert'],r"\3",ct['default'])),CLEAR],
         # Established
-        [20,"","",re.compile(BOL+r"((?:[0-9]+\.){3}[0-9]+\ +)([0-9]+)((?:\ +[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +))([0-9ywdh: ]+)(\ +Establ.*)",flags=re.M),r"\1"+ct['address']+r"\2"+ct['important_value']+r"\3"+ct['default']+r"\4"+ct['general_value']+r"\5"+ct['good']+r"\6"+ct['default'],CLEAR],
+        [20,"","",re.compile(BOL+r"((?:[0-9]+\.){3}[0-9]+\ +)([0-9]+)((?:\ +[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +))([0-9ywdh: ]+)(\ +Establ.*)"), "".join((r"\1",ct['address'],r"\2",ct['important_value'],r"\3",ct['default'],r"\4",ct['general_value'],r"\5",ct['good'],r"\6",ct['default'])),CLEAR],
         # from neighbor
-        [20,"","",re.compile(BOL+r"((?:[0-9]+\.){3}[0-9]+\ +)([0-9]+)((?:\ +[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +))([0-9ywdh:]+)(\ +(?:[0-9]+\/)+.*)",flags=re.M),r"\1"+ct['address']+r"\2"+ct['important_value']+r"\3"+ct['default']+r"\4"+ct['general_value']+r"\5"+ct['good']+r"\6"+ct['default'],CLEAR],
+        [20,"","",re.compile(BOL+r"((?:[0-9]+\.){3}[0-9]+\ +)([0-9]+)((?:\ +[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +))([0-9ywdh:]+)(\ +(?:[0-9]+\/)+.*)"), "".join((r"\1",ct['address'],r"\2",ct['important_value'],r"\3",ct['default'],r"\4",ct['general_value'],r"\5",ct['good'],r"\6",ct['default'])),CLEAR],
         # Active, Connect, or Idle
-        [20,"","",re.compile(BOL+r"((?:[0-9]+\.){3}[0-9]+\ +)([0-9]+)((?:\ +[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +))([0-9ywdh]+)(\ +(?:Active|Connect|Idle).*)",flags=re.M),r"\1"+ct['address']+r"\2"+ct['important_value']+r"\3"+ct['default']+r"\4"+ct['general_value']+r"\5"+ct['alert']+r"\6"+ct['default'],CLEAR],
+        [20,"","",re.compile(BOL+r"((?:[0-9]+\.){3}[0-9]+\ +)([0-9]+)((?:\ +[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +)(?:[0-9]+\ +))([0-9ywdh]+)(\ +(?:Active|Connect|Idle).*)"), "".join((r"\1",ct['address'],r"\2",ct['important_value'],r"\3",ct['default'],r"\4",ct['general_value'],r"\5",ct['alert'],r"\6",ct['default'])),CLEAR],
         # Session timeout warning
         [20,"timeoutwarn","",re.compile(BOL+r"\x07Warning: session will be closed in [0-9]{1,2} (?:minutes?|seconds) if there is no activity.*")],
     ]
